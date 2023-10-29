21ead5e83282deb1a6a5c4a00fc603fae1c3f7aa
StevenG083 <stevengarrett083@gmail.com>
2023-10-28 20:49:03 -0400

    Added Stroke and Fill functionality

--- Week9Program.java
+++ Week9Program.java
@@ -15,6 +15,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import java.util.Objects;
 
 public class Week9Program extends Application {
 	
@@ -24,8 +25,12 @@ public class Week9Program extends Application {
 		Week9Program a = new Week9Program( );
 		ArrayList< String > commands = a.read( getParameters( ).getRaw( ).get( 0 ) );
 		Pane pane = new Pane( );
-
-
+		Color dBackground = Color.BLACK;
+		Color dStroke = Color.rgb( 127, 244, 16 );
+		Color dFill = Color.BLACK;
+		Color stroke = null;
+		Color fill = null;
+	
 		Scene scene = null;
 		int j = 0;
 
@@ -45,7 +50,7 @@ public class Week9Program extends Application {
 		}
 		
 		for ( int i = 0; i < commands.size( ); i++ ) {
-			
+
 			if ( commands.get( i ).equals( "SIZE" ) ) {
 				
 				try {
@@ -54,12 +59,49 @@ public class Week9Program extends Application {
 					i++;
 					Double h = Double.parseDouble( commands.get( i ) );
 					scene = new Scene( pane, w, h );
-					scene.setFill( Color.BLACK );
+					scene.setFill( dBackground );
 				} catch ( NumberFormatException e ) {
 					System.out.println( "There's an error with the SIZE command" );
 				}
 			
 			}
+
+			if ( commands.get( i ).equals( "FILL" ) ) {
+
+				 try {
+					i++;
+					int red = Integer.parseInt( commands.get( i ) );
+					i++;
+					int green = Integer.parseInt( commands.get( i ) );
+					i++;
+					int blue = Integer.parseInt( commands.get( i ) );
+
+					fill = Color.rgb( red, green, blue );
+				} catch ( NumberFormatException e ) {
+					System.out.println( "There's an error with a FILL command" );
+				} catch ( IllegalArgumentException e ) {
+					System.out.println( "That's not a valid color!" );
+				}
+			}
+
+			if ( commands.get( i ).equals( "STROKE" ) ) {
+	
+				 try {
+					i++;
+					int red = Integer.parseInt( commands.get( i ) );
+					i++;
+					int green = Integer.parseInt( commands.get( i ) );
+					i++;
+					int blue = Integer.parseInt( commands.get( i ) );
+
+					stroke = Color.rgb( red, green, blue );
+				} catch ( NumberFormatException e ) {
+					System.out.println( "There's an error with a FILL command" );
+				} catch ( IllegalArgumentException e ) {
+					System.out.println( "That's not a valid color!" );
+				}
+			}
+
 
 			if ( commands.get( i ).equals( "LINE" ) ) {
 				
@@ -73,7 +115,7 @@ public class Week9Program extends Application {
 					i++;
 					Double y2 = Double.parseDouble( commands.get( i ) );
 					Line line = new Line( x1, y1, x2, y2 );
-					line.setStroke( Color.rgb( 127, 244, 16 ) );
+					line.setStroke( Objects.isNull( stroke ) ? dStroke: stroke );
 					pane.getChildren( ).add( line ); 
 				} catch ( NumberFormatException e ) {
 					System.out.println( "There's an error with a LINE command" );
@@ -90,7 +132,7 @@ public class Week9Program extends Application {
 					i++;
 					Double r = Double.parseDouble( commands.get( i ) );
 					Circle circle = new Circle( x, y, r );
-					circle.setStroke( Color.rgb( 127, 244, 16 ) );
+					circle.setStroke( Objects.isNull( stroke ) ? dStroke: stroke );
 					pane.getChildren( ).add( circle );
 				} catch ( NumberFormatException e ) {
 					System.out.println( "There's an error with a CIRCLE command" );
@@ -110,8 +152,8 @@ public class Week9Program extends Application {
 					i++;
 					Double h = Double.parseDouble( commands.get( i ) );
 					Rectangle rectangle = new Rectangle( x, y, w, h );
-					rectangle.setFill( Color.BLACK );
-					rectangle.setStroke( Color.rgb( 127, 244, 16 ) );
+					rectangle.setFill( Objects.isNull( fill ) ? dFill: fill );
+					rectangle.setStroke( Objects.isNull( stroke ) ? dStroke: stroke );
 					pane.getChildren( ).add( rectangle );
 				} catch ( NumberFormatException e ) {
 					System.out.println( "There's an error with a RECTANGLE command" );
@@ -145,8 +187,8 @@ public class Week9Program extends Application {
 					if ( commands.get( i ).equals( "ROUND" ) ) {
 						arc.setType( ArcType.ROUND );
 					}
-					arc.setFill( Color.BLACK );
-					arc.setStroke( Color.rgb( 127, 244, 16 ) );
+					arc.setFill( Objects.isNull( fill ) ? dFill: fill );
+					arc.setStroke( Objects.isNull( stroke ) ? dStroke: stroke );
 					pane.getChildren( ).add( arc );
 					i--;
 				} catch ( NumberFormatException e ) {
@@ -173,8 +215,8 @@ public class Week9Program extends Application {
 							polyList.add( Double.parseDouble( commands.get( i ) ) );
 							i++;
 						}
-					polygon.setFill( Color.BLACK );
-					polygon.setStroke( Color.rgb( 127, 244, 16 ) );
+					polygon.setFill( Objects.isNull( fill ) ? dFill: fill );
+					polygon.setStroke( Objects.isNull( stroke ) ? dStroke: stroke );
 					polygon.getPoints().addAll( polyList );
 					pane.getChildren( ).add( polygon );
 					polyList.clear( );
@@ -213,7 +255,7 @@ public class Week9Program extends Application {
 
 					String str = sb.toString( );
 					Text text = new Text( x, y, str );
-					text.setStroke( Color.rgb( 127, 244, 16 ) );
+					text.setStroke( Objects.isNull( stroke ) ? dStroke: stroke );
 					pane.getChildren( ).add( text );
 					i--;
 				} catch ( NumberFormatException e ) {
